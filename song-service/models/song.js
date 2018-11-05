4d96a52aceb38418f1ec3800cefd426a0344a52b
Unknown <ammar.cse@gmail.com>
2018-11-05 20:03:38 +0200

    add passing test for song model

--- song-service/models/song.js
+++ song-service/models/song.js
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 const songSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    uri: { type: String, required: true },
+    artist: { type: String },
+    length: { type: String },
+    genre: { type: String },
+    format: { type: String },
+    publisher: { type: String }
 });
  
 export default mongoose.model('Song', songSchema);
