616d476d9ac95d01e0c3e6fb3169474425008a56
Unknown <ammar.cse@gmail.com>
2018-11-05 20:43:37 +0200

    add initial seeding array for songs

---
+++ song-service/src/models/db/seeds/songs.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title: 'Rain',
+        uri: '/some-folder/some-file.mp3',
+        artist: '',
+        length: '1:03',
+        genre: 'Soothing',
+        format: 'mp3',
+        publisher: '' 
+    },
+    {
+        title: 'MetaProgramming in Ruby',
+        uri: '/some-folder/some-file.mp3',
+        artist: '',
+        length: '1:03',
+        genre: 'Educational',
+        format: 'mp3',
+        publisher: '' 
+    },
+    {
+        title: 'Waves',
+        uri: '/some-folder/some-file.mp3',
+        artist: '',
+        length: '1:03',
+        genre: 'Soothing',
+        format: 'mp3',
+        publisher: '' 
+    },
+    {
+        title: 'Surah Al Asr',
+        uri: '/some-folder/some-file.mp3',
+        artist: '',
+        length: '1:03',
+        genre: 'Spiritual',
+        format: 'mp3',
+        publisher: '' 
+    }
+];
