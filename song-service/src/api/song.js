4347a058e1b385d037876c51d55339ea5ab78b9e
Unknown <ammar.cse@gmail.com>
2018-11-05 22:01:25 +0200

    add passing code for song api with search term

--- song-service/src/api/song.js
+++ song-service/src/api/song.js
@@ -3,17 +3,18 @@ import to from 'await-to-js';
 import Song from '../models/song';
 
 function api(app){
-  app.get('/songs', async (request, response, next) => {
-    const [error, songs] = await to(Song.find({}, '-_id -__v'));
-    response.send(httpStatus.OK, songs);  
-    next();
-  })
+    app.get('/songs', async (request, response, next) => {
+        const [error, songs] = await to(Song.find({}, '-_id -__v'));
+        response.send(httpStatus.OK, songs);  
+        next();
+    });
 
-  /*app.get('/movies/premieres', (req, res, next) => {
-    repo.getMoviePremiers().then(movies => {
-      res.status(httpStatus.OK).json(movies)
-    }).catch(next)
-  })*/
+    app.get('/songs/:genre', async (request, response, next) => {
+        const {genre} = request.params;
+        const [error, songs] = await to(Song.find({ genre }, '-_id -__v'));
+        response.send(httpStatus.OK, songs);  
+        next();
+    });
 }
 
 export default api;
