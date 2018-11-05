07696f7fcb0015b4f0a0742ff44791f5bdd34995
Unknown <ammar.cse@gmail.com>
2018-11-05 20:47:01 +0200

    add config for different environments

---
+++ song-service/config/env/production.js
@@ -0,0 +1,5 @@
+export default {
+    dbUri: 'mongodb://localhost/songs_prod',
+    appName: 'aula-assignment-prod',
+    appVersion: '1.0.0'
+}
---
+++ song-service/config/env/test.js
@@ -0,0 +1,5 @@
+export default {
+    dbUri: 'mongodb://localhost/songs_test',
+    appName: 'aula-assignment-test',
+    appVersion: '1.0.0'
+}
