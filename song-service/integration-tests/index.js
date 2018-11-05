aa641c9a4ef33ad55a07543e10ebc613a31e4094
Unknown <ammar.cse@gmail.com>
2018-11-05 20:43:53 +0200

    add full get songs integration test

---
+++ song-service/integration-tests/index.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import to from 'await-to-js';
+import startService from '../src/service';
+import seedData from '../src/models/db/seeds/songs.js';
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('songs api', () => {
+  describe('GET /songs', () => {
+    it('responds with seeded data', async() => {
+        const service = await startService(); 
+        
+        const [error, response] = await to(chai.request(service).get('/songs'));
+        response.should.have.status(200);
+        response.body.should.be.a('array');
+        response.body.should.be.eql(seedData);
+      });
+  });
+});
