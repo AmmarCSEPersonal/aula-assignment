50c6eeb6960307d300988d62a13b87a8d9fe8c77
Unknown <ammar.cse@gmail.com>
2018-11-05 22:18:10 +0200

    add passing code for user model test

--- user-service/models/user.js
+++ user-service/models/user.js
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
+    username: { type: String, required: true }
+    //we dont need these for now but any other user information
+    //should be minimal and directly tied to the user himself since this is a service
+    /*age: { type: String, required: true },
+    sex: { type: String },
+    yada: { type: String },
+    yada: { type: String }*/
 });
  
 export default mongoose.model('User', userSchema);
