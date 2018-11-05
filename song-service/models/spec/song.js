616182c59f702612a26aa9254cee3822c3ae6274
Unknown <ammar.cse@gmail.com>
2018-11-05 20:14:02 +0200

    add test genre or format should be type enum

--- song-service/models/spec/song.js
+++ song-service/models/spec/song.js
@@ -8,8 +8,15 @@ describe('song', () => {
         const song = new Song();
 
         const [result] = await to(song.validate());
-        expect(result.errors.title).to.exist;
-        expect(result.errors.uri).to.exist;
+        expect(result.errors.title.kind).to.eql('required');
+        expect(result.errors.uri.kind).to.eql('required');
+    });
+    it('should be only allow valid enum type for genre or format', async() => {
+        const song = new Song({genre: 'some invalid enum', format: 'some invalid enum'});
+
+        const [result] = await to(song.validate());
+        expect(result.errors.genre.kind).to.eql('enum');
+        expect(result.errors.format.kind).to.eql('enum');
     });
 });
 
