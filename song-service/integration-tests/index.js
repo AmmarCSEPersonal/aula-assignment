7460f602e23326535e6549cd0623d2a80bd34028
Unknown <ammar.cse@gmail.com>
2018-11-05 22:02:02 +0200

    add integration test for song api with search term

--- song-service/integration-tests/index.js
+++ song-service/integration-tests/index.js
@@ -8,14 +8,22 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('songs api', () => {
-  describe('GET /songs', () => {
-    it('responds with seeded data', async() => {
-        const service = await startService(); 
-        
-        const [error, response] = await to(chai.request(service).get('/songs'));
-        response.should.have.status(200);
-        response.body.should.be.a('array');
-        response.body.should.be.eql(seedData);
-      });
-  });
+    describe('GET /songs', () => {
+        it('responds with seeded data', async() => {
+            const service = await startService(); 
+            
+            const [error, response] = await to(chai.request(service).get('/songs'));
+            response.should.have.status(200);
+            response.body.should.be.a('array');
+            response.body.should.be.eql(seedData);
+        });
+        it('responds with filtered seeded data when genre passed in', async() => {
+            const service = await startService(); 
+            
+            const [error, response] = await to(chai.request(service).get('/songs/Soothing'));
+            response.should.have.status(200);
+            response.body.should.be.a('array');
+            response.body.should.be.eql(seedData.filter(seed => seed.genre == 'Soothing'));
+        });
+    });
 });
