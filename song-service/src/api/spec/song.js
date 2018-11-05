ff27510e40781eb0950b1b2b1d4a6092ed1067a9
Unknown <ammar.cse@gmail.com>
2018-11-05 22:01:02 +0200

    add test for song api with search term

--- song-service/src/api/spec/song.js
+++ song-service/src/api/spec/song.js
@@ -6,14 +6,24 @@ import startService from '../../service';
 const should = chai.should();
 chai.use(chaiHttp);
 
+//in real world, we shouldnt be naive and we should actually handle
+//other status codes such as 400+(authentication, not found if search term brings no results, etc)
+//and gracefully handle 500s
 describe('songs api', () => {
-  describe('GET /songs', () => {
-    it('responds with OK', async() => {
-        const service = await startService(); 
-        
-        const [error, response] = await to(chai.request(service).get('/songs'));
-        response.should.have.status(200);
-        response.body.should.be.a('array');
-      });
-  });
+    describe('GET /songs', () => {
+        it('responds with OK', async() => {
+            const service = await startService(); 
+
+            const [error, response] = await to(chai.request(service).get('/songs'));
+            response.should.have.status(200);
+            response.body.should.be.a('array');
+        });
+        it('responds with OK when search term included', async() => {
+            const service = await startService(); 
+
+            const [error, response] = await to(chai.request(service).get('/songs/Soothing'));
+            response.should.have.status(200);
+            response.body.should.be.a('array');
+        });
+    });
 });
