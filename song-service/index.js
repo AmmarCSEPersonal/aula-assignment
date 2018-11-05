3a1aaa3c2b2225631c5e8c6ba72ff68b47c91bb9
Unknown <ammar.cse@gmail.com>
2018-11-05 21:51:19 +0200

    add yarn start script

---
+++ song-service/index.js
@@ -0,0 +1,6 @@
+import startService from './src/service';
+
+startService();
+//other things you can do here is:
+//instantiate registration and communication messaging bus
+//register and start third-party tools used to address cross-cutting concerns
