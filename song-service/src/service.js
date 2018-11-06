18ac90652d7e56c5957c793d8a574f75927871ec
Unknown <ammar.cse@gmail.com>
2018-11-06 06:14:29 +0200

    remove static ports used by services

--- song-service/src/service.js
+++ song-service/src/service.js
@@ -19,7 +19,7 @@ async function launchSequence(service) {
 
 async function listen(service){
     api(service, {});
-    return await service.listen(8080);
+    return await service.listen();
 }
 
 export default async function startService(){
